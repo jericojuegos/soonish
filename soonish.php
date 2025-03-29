74fb4a8e4bac7619b7a89c7ba58f6200939f088b
Jerico Juegos <juegosdotjerico@gmail.com>
2025-03-29 18:16:10 +0800

    feat(admin): add coming soon mode notification in admin bar

--- soonish.php
+++ soonish.php
@@ -29,6 +29,9 @@ class Soonish {
         add_action('admin_init', array($this, 'register_settings'));
         add_action('template_redirect', array($this, 'display_coming_soon_page'));
         add_action('admin_enqueue_scripts', array($this, 'admin_scripts'));
+        add_action('admin_bar_menu', array($this, 'add_admin_bar_notification'), 100);
+        add_action('admin_head', array($this, 'admin_bar_notification_css'));
+        add_action('wp_head', array($this, 'admin_bar_notification_css'));
     }
 
     public function add_admin_menu() {
@@ -71,6 +74,49 @@ class Soonish {
 
         include_once plugin_dir_path(__FILE__) . 'templates/coming-soon.php';
         exit;
+    }
+    
+    /**
+     * Add notification to the admin bar when Coming Soon Mode is enabled
+     *
+     * @param WP_Admin_Bar $wp_admin_bar Admin bar instance
+     */
+    public function add_admin_bar_notification($wp_admin_bar) {
+        // Only show if Coming Soon Mode is enabled
+        if (!get_option('soonish_enabled', false)) {
+            return;
+        }
+        
+        // Add the main notification node
+        $wp_admin_bar->add_node(array(
+            'id'    => 'soonish-notification',
+            'title' => '<span class="ab-icon"></span> Coming Soon Mode is enabled',
+            'href'  => admin_url('options-general.php?page=soonish'),
+            'meta'  => array(
+                'class' => 'soonish-notification',
+            ),
+        ));
+    }
+
+    public function admin_bar_notification_css() {
+        ?>
+        <style>
+            #wpadminbar .soonish-notification {
+                background-color: #d63638;
+            }
+            
+            #wpadminbar .soonish-notification .ab-item {
+                color: #fff !important;
+                font-weight: bold;
+            }
+            
+            #wpadminbar .soonish-notification .ab-icon:before {
+                content: '\f534';
+                top: 2px;
+                color: #fff !important;
+            }
+        </style>
+        <?php
     }
 }
 
